011d78e0394c353c40dbafe085149ff28b8e6deb
honoyr <dinis.gonor@gmail.com>
2019-03-27 17:02:37 -0700

    linked list task - correct - put to str result

--- resources/ex00/precious.c
+++ resources/ex00/precious.c
@@ -18,67 +18,46 @@
 
 char *precious(int *text, int size)
 {
-	struct s_node *cs = NULL;
-	struct s_node *tmp = NULL;
+	struct s_node	*cs = NULL;
+	struct s_node	*tmp = NULL;
+	char			*str = NULL;
 
+	
 	if (!(cs = (struct s_node*)malloc(sizeof(struct s_node))))
+		return NULL;
+	if(!(str = (char*)malloc(sizeof(char) * (size + 1))))
 		return NULL;
 	tmp  = cs;
 	for(int i = 0; i < 80; i++)
 	{
 		tmp->c = CS[i];
-		// printf("CHAR = %c\n", tmp->c);
 		tmp->next = (struct s_node*)malloc(sizeof(struct s_node));
 		tmp->next->next = NULL;
 		tmp->next->prev = tmp;
-		// printf("CHAR = %c\n", tmp->prev->c);
 		tmp = tmp->next;
 
 	}
-	// printf("CHAR = %c\n", tmp->c);
 	cs->prev = tmp->prev;
 	tmp = tmp->prev;
 	tmp->next = cs;
 	tmp = cs;
-	// for(int i = 0; i < 80; i++)
-	// {
-		
-	// 	tmp = tmp->next;
-	// 	printf("CHAR = %c\n", tmp->c);
-	// }
-	// printf("\n");
-	// tmp = cs;
-	// printf("SIZE = %i\n", size);
-	// for(int i = 0; i < (size); i++)
-	// {
-	// 	printf("%i\n", text[i]);
-	// }
-	tmp = cs;
-	// printf("\nCHAR =%c\n", tmp->c);
+	printf("here\n");
 	for(int i = 0; i < size; i++)
 	{
-		// printf("%i\n\n", text[i]);
 		if (text[i] < 0)
 		{
-			// printf("\nCHAR1 =%c\n", tmp->c);
-			// printf("\nNUM = %i\n", text[i]);
 			for (int j = text[i]; j < 0; j++)
-			{
-				printf("\nNUM1 =%i\n", j);
 				tmp = tmp->prev;
-			}
+			str[i] = tmp->c;
 			printf("%c", tmp->c);
 		}
 		else
 		{
 			for(int k = 0; k < text[i]; k++)
-			{
-				// printf("\nCHAR2 =%c\n", tmp->c);
 				tmp = tmp->next;
-			}
+			str[i] = tmp->c;
 			printf("%c", tmp->c);
 		}
 	}
-	printf("\n");
-	return "OK";
+	return str;
 }
